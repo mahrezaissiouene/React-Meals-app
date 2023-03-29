e0298df6b03994d73567e00056f6198fda8125bd
mahrezaissiouene <mahrez.aissiouene@gmail.com>
2023-03-29 02:36:04 -0400

    add reducer to manage cart items

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,12 +1,44 @@
-import React from 'react'
+import React , {useReducer } from 'react'
 import Cartcontext from './cartcontext';
+
+const defaultCartState = {
+  items: [],
+  totalAmount: 0
+}
+
+{ /* reducer function */ }
+const cartReducer = (state , action ) => {
+if ( action.type === 'ADD_ITEM'  ) {
+  const updatedItems = state.items.concat(action.item); { /* state represente cartState , action.item grab the sent item , concat create a new array with adding the new item */ }
+  const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount ;
+  return {
+    items : updatedItems ,
+    totalAmount : updatedTotalAmount
+  };
+}
+
+if ( action.type === 'REMOVE_ITEM'  ) {
+
+
+}
+
+
+
+  return ( defaultCartState  ); 
+}
+
 
 function CartProvider(props) {
 
+  const [cartState , dispatchCartAcion ] = useReducer(cartReducer , defaultCartState); { /* reducer function , initial value */ }
+
   const addItemTocartHandler = item => {
+    dispatchCartAcion( {type : 'ADD_ITEM' , item : item } );
   };
 
-  const removeItemFromcartHandler = id => { };
+  const removeItemFromcartHandler = id => {
+    dispatchCartAcion( {type : 'REMOVE_ITEM' , id : id } );
+   };
 
 
   const cartcontext = {
