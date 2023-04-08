02fc01de07df370334654e98e400c55ba60552d1
mahrezaissiouene <mahrez.aissiouene@gmail.com>
2023-04-08 01:12:27 -0400

    handling the loading state

--- src/components/Meals/AvailablesMeals.js
+++ src/components/Meals/AvailablesMeals.js
@@ -1,37 +1,45 @@
 import classes from "./AvailablesMeals.module.css";
 import MealsItem from "./MealsItem/MealsItem";
 import Card from "../UI/Card";
-import { useEffect , useState } from "react";
-
+import { useEffect, useState } from "react";
 
 const AvailablesMeals = () => {
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setisLoading] = useState(true);
 
-
-  const [meals , setMeals ] = useState([]);
-
-
-  useEffect( ()=>{
-    const fetchmeals = async ()=> {
-      const response = await fetch('https://react-meals-6a3c5-default-rtdb.firebaseio.com/meals.json')
+  useEffect(() => {
+    const fetchmeals = async () => {
+      const response = await fetch(
+        "https://react-meals-6a3c5-default-rtdb.firebaseio.com/meals.json"
+      );
       const responseData = await response.json();
 
       const loadedMeals = [];
 
-      for ( const  key in responseData ) {
-        loadedMeals.push( {
-          id : key ,
-          name : responseData[key].name ,
-          description : responseData[key].name ,
-          price : responseData[key].price
-        })
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].name,
+          price: responseData[key].price,
+        });
       }
 
       setMeals(loadedMeals);
-     
-    }
-    fetchmeals();
+    };
 
-  } , [] )
+    setisLoading(false);
+
+    fetchmeals();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes.loadingmeals}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
 
   const mealslist = meals.map((meal) => (
     <MealsItem
